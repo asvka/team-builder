105e2b0d86e06b500eb0b369324b4fb631b685e9
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-01-22 19:33:50 -0800

    MVP, form returns newMember data. Also, styling was added.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,87 @@
 import React, { useState } from 'react';
 import './App.css';
 import './Components/MemberForm';
-import './Components/Member'
-import MemberForm from './Components/MemberForm';
+import './Components/Member';
 import Member from './Components/Member';
-import { ReactDOM } from 'react-dom';
 
 
-function App() {
-  const [members, setMembers] = useState({
+function App () {
+  const [members, setMembers] = useState([{
     name: "Anthony Hernandez",
     email: "amhwaa@gmail.com",
-    role: "Full-Stack Web Dev"
-
-});
+    role: "Full-Stack Web Developer"
+}]);
 console.log(members);
+
+  const MemberForm = props => {
+  const [member, setMember] = useState({
+      name: "",
+      email: "",
+      role: "",
+  });
+
+  const handleChanges = event => {
+      setMember({ ...member, [event.target.name]: event.target.value });
+      console.log(member);
+  };
+
+  const submitHandler = event => {
+      event.preventDefault();
+      const newMember ={
+          ...member,
+          id: Date.now()
+      };
+      addNewMember(newMember);
+      console.log(newMember);
+      setMember({ name: "", email: "", role: "" });
+  };
+  return (
+  <div>
+    <form className="form" onSubmit={submitHandler}>
+      {/* <h1>{member.name} </h1> */}
+      <label htmlFor="name">Name</label>
+      <input
+        onChange={handleChanges}
+        id="name"
+        type="text"
+        name="name"
+        value={member.name}
+      />
+
+      <label htmlFor="email">Email Address </label>
+      <input
+        onChange={handleChanges}
+        id="email"
+        type="text"
+        name="email"
+        value={member.email}
+      />
+
+      <label htmlFor="role">Role </label>
+      <select
+        onChange={handleChanges}
+        id="role"
+        type="select"
+        name="role"
+        value={member.role}
+      >
+        <option> </option>
+        <option value ='Full-Stack Web Developer'>Full-Stack Web Developer</option>
+        <option value ='Data Scientist'>Data Scientist</option>
+        <option value ='Buttered Toast'>Buttered Toast</option>
+</select>
+      <button name="submitButton" type="submit">
+        Join!
+      </button>
+    </form>
+  </div>
+);
+  }
+
 
   const addNewMember = member => {
     const newMember = setMembers([...members, member]);
+    console.log(newMember);
   }
 
   return (
@@ -30,5 +94,4 @@ console.log(members);
     </div>
   );
 }
-export default App;
-
+export default App
--- team-builder/src/Components/Member.js
+++ team-builder/src/Components/Member.js
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function Member (props) {
-    const [member, setMember] = useState({});
-    useEffect(()=> {
-        setMember(props.members);
-    }, [props.members])
+
     return (
         <div>
-            <h2>{member.name}</h2>
-            <p>{member.email}</p>
-            <p>{member.role}</p>
+            {props.members.map(member => (
+                <div className="member-list">
+                    <h2>{member.name}</h2>
+                    <p>{member.email}</p>
+                    <p>{member.role}</p>
+                </div>
+            ))}
         </div>
-    )
-
-//   return (
-//     <div>
-//       {props.member.map(member => (
-//         <div className="member" key={member.id}>
-//           <h2>{member.name}</h2>
-//           <p>{member.email}</p>
-//           <p>{member.role}</p>
-//         </div>
-//       ))}
-//     </div>
-
-//   );
+    );
 };
 
